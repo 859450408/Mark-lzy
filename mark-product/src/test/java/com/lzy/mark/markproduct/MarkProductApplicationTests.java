b3e0c5b2f56cf483010e695cd90accc339deb731
859450408 <859450408@qq.com>
2021-11-27 20:47:41 +0800

    1.0.1 微服务增删改查逆向完毕，测试无异常

--- mark-product/src/test/java/com/lzy/mark/markproduct/MarkProductApplicationTests.java
+++ mark-product/src/test/java/com/lzy/mark/markproduct/MarkProductApplicationTests.java
@@ -1,13 +1,31 @@
 package com.lzy.mark.markproduct;
 
+import com.lzy.mark.markproduct.entity.BrandEntity;
+import com.lzy.mark.markproduct.entity.SpuInfoEntity;
+import com.lzy.mark.markproduct.service.BrandService;
+import com.lzy.mark.markproduct.service.SpuInfoService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MarkProductApplication.class)
 class MarkProductApplicationTests {
-
+    @Autowired
+    BrandService brandService;
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setBrandId(1L);
+        brandEntity.setDescript("很贵");
+//        brandEntity.setName("IPhone 13 pro max");
+//        brandService.save(brandEntity);
+//        System.out.println("success");
+        brandService.updateById(brandEntity);
     }
 
 }
