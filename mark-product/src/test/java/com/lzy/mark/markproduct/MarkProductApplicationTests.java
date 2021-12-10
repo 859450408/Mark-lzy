6bc14ef98c1f0c7149726d1c0fcec4da558ddece
lzy <51665125+859450408@users.noreply.github.com>
2021-12-11 03:40:34 +0800

    Update MarkProductApplicationTests.java

--- mark-product/src/test/java/com/lzy/mark/markproduct/MarkProductApplicationTests.java
+++ mark-product/src/test/java/com/lzy/mark/markproduct/MarkProductApplicationTests.java
@@ -22,38 +22,6 @@ class MarkProductApplicationTests {
     @Autowired
     BrandService brandService;
 
-    @Test
-    void contextLoads() {
-        BrandEntity brandEntity = new BrandEntity();
-        brandEntity.setBrandId(1L);
-        brandEntity.setDescript("很贵");
-//        brandEntity.setName("IPhone 13 pro max");
-//        brandService.save(brandEntity);
-//        System.out.println("success");
-        brandService.updateById(brandEntity);
-    }
-
-    @Test
-    void OSSTest(){
-        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
-        String endpoint = "https://oss-cn-beijing.aliyuncs.com";
-// 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5t7KmmhEaW4fSCDfTRHB";
-        String accessKeySecret = "UBzFSLPUT78EfFfMl9JYmAl1orureP";
-// 填写Bucket名称，例如examplebucket。
-        String bucketName = "mark-lzy";
-// 填写文件名。文件名包含路径，不包含Bucket名称。例如exampledir/exampleobject.txt。
-        String objectName = "D:\\微信图片.jpg";
-
-// 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-
-        String content = "Hello OSS";
-        ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(content.getBytes()));
-
-// 关闭OSSClient。
-        ossClient.shutdown();
-
-    }
+  
 
 }
